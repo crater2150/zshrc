9205a8ce67d2b5430caae6aa2d216d34b26487fa
crater2150 <crater@qwertyuiop.de>
2019-02-11 16:25:00 +0100

    Add zunit

--- zplug.zsh
+++ zplug.zsh
@@ -2,5 +2,13 @@ source $(zdotfile zplug/init.zsh)
 
 zplug "MichaelAquilina/zsh-you-should-use"
 
+zplug 'molovo/revolver', \
+  as:command, \
+  use:revolver
+zplug 'zunit-zsh/zunit', \
+  as:command, \
+  use:zunit, \
+  hook-build:'./build.zsh'
+
 zplug load
 
