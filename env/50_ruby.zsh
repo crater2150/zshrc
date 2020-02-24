3d1da888c6c64c7e477c46daec79b52ee53de7f4
crater2150 <crater@qwertyuiop.de>
2020-02-24 09:19:10 +0000

    Make exists() available in zshenv

--- env/50_ruby.zsh
+++ env/50_ruby.zsh
@@ -1,2 +1,4 @@
-RUBY_VERSION=${$(gem environment gemdir):t}
-export PATH="$PATH:$HOME/.gem/ruby/$RUBY_VERSION/bin"
+if exists gem; then
+	RUBY_VERSION=${$(gem environment gemdir):t}
+	export PATH="$PATH:$HOME/.gem/ruby/$RUBY_VERSION/bin"
+fi
--- zshenv
+++ zshenv
@@ -1,5 +1,6 @@
 export ZDOTDIR="${$(readlink $HOME/.zshenv):h:a}"
 
+exists() { command -v "$@" >/dev/null }
 for i in $ZDOTDIR/env/*.zsh; do
 	. $i
 done
