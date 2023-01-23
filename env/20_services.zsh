77214e5f5eaefcd5b4a871483629f9c327b1c047
crater2150 <crater@qwertyuiop.de>
2023-01-23 15:31:22 +0100

    Add env vars

--- env/20_services.zsh
+++ env/20_services.zsh
@@ -1,3 +1,4 @@
 if [[ $DISPLAY && $UID != 0 ]]; then
 	export SVDIR="$HOME/.local/session_service"
+	export SVDIR_TEMPLATES="$HOME/.service-available"
 fi
--- env/50_nodejs.zsh
+++ env/50_nodejs.zsh
@@ -1,3 +1,4 @@
 export NPM_PACKAGES="${XDG_DATA_HOME}/npm-packages"
 export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 export PATH="$PATH:$NPM_PACKAGES/bin"
+export MANPATH="${MANPATH}:$NPM_PACKAGES/share/man"
