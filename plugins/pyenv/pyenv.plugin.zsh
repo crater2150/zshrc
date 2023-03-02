0cae13e4c167ed5844c69e7ca5855a4c29124141
crater2150 <crater@qwertyuiop.de>
2023-03-02 16:11:04 +0100

    pyenv: install plugins

--- plugins/pyenv/pyenv.plugin.zsh
+++ plugins/pyenv/pyenv.plugin.zsh
@@ -18,6 +18,11 @@ else
     pyenv() {
 	echo -n "pyenv is not installed. Install now? [yn] "; read -q || return
 	git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT
+	git clone https://github.com/pyenv/pyenv-update.git $PYENV_ROOT/plugins/pyenv-update
+	git clone https://github.com/pyenv/pyenv-virtualenv.git $PYENV_ROOT/plugins/pyenv-virtualenv
+	git clone https://github.com/jawshooah/pyenv-default-packages.git $PYENV_ROOT/plugins/pyenv-default-packages
+	git clone https://github.com/fizista/pyenv-choice.git $PYENV_ROOT/plugins/pyenv-choice
+	echo ptpython > $PYENV_ROOT/default-packages
 	_init_pyenv
     }
 fi
