85ec31479eb0d466edf5059ada73a25e93d61ea2
crater2150 <crater@qwertyuiop.de>
2023-08-21 11:52:05 +0200

    env: set rust paths

--- env/50_rust.zsh
+++ env/50_rust.zsh
@@ -1,3 +1,6 @@
+export RUSTUP_HOME="${XDG_DATA_HOME}/rustup"
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+export PATH="${CARGO_HOME}/bin:${PATH}"
 if which rustup &>/dev/null && [[ -n $(rustup show active-toolchain) ]]; then
 	export RUST_SRC_PATH=$(rustup run stable rustc --print sysroot)/lib/rustlib/src/rust/src
 fi
